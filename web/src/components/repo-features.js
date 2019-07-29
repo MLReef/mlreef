cdebd2c79006cfe9732aead325201dcf242fbf66
cpmlreef <cp@mlreef.com>
2019-07-29 10:23:59 +0000

    Feature/add responsiveness data repo overview

--- web/src/components/repo-features.js
+++ web/src/components/repo-features.js
@@ -6,39 +6,37 @@ export default class RepoFeatures extends React.Component{
     render() {
         return <div id="repo-features">
             <div>
-                <button className="white-button">
-                    Master 
+                <button class="white-button">
+                    Master
                     <img id="leftfeature-image" src={arrow_down_blue_01} alt=""/>
                 </button>                    
 
-                <button className="white-button">
+                <button class="white-button">
                     <img id="plus" src={plus_01} alt=""/>
                     <img id="leftfeature-image" src={arrow_down_blue_01} alt=""/>
                 </button>                   
 
-                <button className="blue-button">
+                <button class="blue-button">
                     <p>Data Visualisation</p>
                 </button>
 
-                <button className="blue-button">
+                <button class="blue-button">
                     <p>Data Pipeline</p>
                 </button>
             </div>
             <div>
-                <button className="white-button">
+                <button class="white-button">
                     <p>History</p>
                 </button>                    
 
-                <button className="white-button">
+                <button class="white-button">
                     <p>Web IDE</p>
                 </button>                   
 
-                <button className="white-button">
+                <button class="white-button">
                     <img id="leftfeature-image" src={arrow_down_blue_01} alt=""/>
                 </button>
             </div>
         </div>
     }
 }
-
-//style={{ align:'right', padding: '0 1% 0 1%'}}
