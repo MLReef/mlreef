3bccaea3d9168e8921b83d224fbd784911823d75
SaathvikT <saathvik1997@gmail.com>
2019-08-04 14:23:01 +0530

    Complete changes commit data-repo

--- web/src/components/file-view.js
+++ web/src/components/file-view.js
@@ -1,80 +1,106 @@
 import React from "react";
-import ProjectContainer from './projectContainer';
+import ProjectContainer from "./projectContainer";
 import "../css/file-view.css";
-import {connect} from 'react-redux';
-import * as fileActions from '../actions/fileActions';
-import {bindActionCreators} from "redux";
-import {Base64} from 'js-base64';
-import Navbar from './navbar';
+import { connect } from "react-redux";
+import * as fileActions from "../actions/fileActions";
+import { bindActionCreators } from "redux";
+import { Base64 } from "js-base64";
+import Navbar from "./navbar";
 
-class FileView extends React.Component{
-    componentDidMount(){
-        var url_string = window.location.href;
-        var url = new URL(url_string);
-        var path = url.searchParams.get("path");
-        
-        if(path){
-            path = path.replace(/\//g, '%2F');
-            path = path + "%2F" + this.props.match.params.file
-        } else {
-            path = this.props.match.params.file;
-        }
+class FileView extends React.Component {
+  componentDidMount() {
+    var url_string = window.location.href;
+    var url = new URL(url_string);
+    var path = url.searchParams.get("path");
 
-        this.props.actions.getFileData(path, this.props.match.params.branch);
+    if (path) {
+      path = path.replace(/\//g, "%2F");
+      path = path + "%2F" + this.props.match.params.file;
+    } else {
+      path = this.props.match.params.file;
     }
 
-    render(){
-        const fileName = this.props.fileData.file_name;
-        const fileSize = this.props.fileData.size;
-        let fileContent = [];
-        let foo;
-        if(this.props.fileData.content){
-            fileContent = Base64.decode(this.props.fileData.content).split("\n");
-            foo = this.props.fileData.content;
-        }
-    
-        return (
-            <div>
-                <Navbar/>
-                <ProjectContainer/>
-                <div className="file-container">
-                    <div className="file-container-header">
-                        <p>{fileName} | {fileSize} Bytes</p>
-                    </div>
-                    <div className="file-content">
-                        <table>
-                            <tbody>
-                                {fileContent.map(function(line) {
-                                    console.log("foo.size:" + foo.size + "; foo.length:" + foo.length);
+    this.props.actions.getFileData(path, this.props.match.params.branch);
+  }
 
-                                    return (
-                                        <tr>
-                                            <td>
-                                                <p>{line}</p>
-                                            </td>
-                                        </tr>
-                                    )
-                                })}
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
+  render() {
+    const fileName = this.props.fileData.file_name;
+    const fileSize = this.props.fileData.size;
+    let fileContent = [];
+    let foo;
+    let extension;
+    if (this.props.fileData.content) {
+      fileContent = Base64.decode(this.props.fileData.content).split("\n");
+      extension = fileName.split(".").pop();
+      foo = this.props.fileData.content;
+    }
+
+    return (
+      <div>
+        <Navbar />
+        <ProjectContainer />
+        <div className="file-container">
+          <div className="file-container-header">
+            <div className="file-info">
+              <p>
+                {fileName} | {fileSize} Bytes
+              </p>
             </div>
-        )
-    }
+            <div className="wrapper">
+              <div className="file-actions">
+                <button className="white-button">History</button>
+                <button className="white-button">Replace</button>
+                <button className="red-button">Delete</button>
+              </div>
+            </div>
+          </div>
+          <div
+            itemProp="text"
+            className="Box-body p-0 blob-wrapper data type-text"
+          >
+            <div className="file-content">
+              {extension === ("png" || "jpg" || "jpeg") ? (
+                <img
+                  src={`data:image/png;base64,${this.props.fileData.content}`}
+                  alt={fileName}
+                />
+              ) : (
+                <table>
+                  <tbody>
+                    {fileContent.map(function(line) {
+                      return (
+                        <tr>
+                          <td>
+                            <p>{line}</p>
+                          </td>
+                        </tr>
+                      );
+                    })}
+                  </tbody>
+                </table>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-function mapStateToProps(state){
-    return {
-        fileData: state.file,
-        project: state.project
-    };
+function mapStateToProps(state) {
+  return {
+    fileData: state.file,
+    project: state.project
+  };
 }
 
 function mapDispatchToProps(dispatch) {
-    return {
-        actions: bindActionCreators(fileActions, dispatch)
-    };
+  return {
+    actions: bindActionCreators(fileActions, dispatch)
+  };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(FileView);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(FileView);
--- web/src/components/repo-features.js
+++ web/src/components/repo-features.js
@@ -3,42 +3,103 @@ import arrow_down_blue_01 from "./../images/arrow_down_blue_01.svg";
 import plus_01 from "./../images/plus_01.svg";
 
 export default class RepoFeatures extends React.Component {
+  state = {
+    isOpen: false,
+    branchSelected: "Master"
+  };
+
+  Branches = ["Master", "feature/28-repo", "feature/41-pipeline"];
+
+  handleBlur = e => {
+    if (
+      e.nativeEvent.explicitOriginalTarget &&
+      e.nativeEvent.explicitOriginalTarget === e.nativeEvent.originalTarget
+    ) {
+      return;
+    }
+
+    if (this.state.isOpen) {
+      setTimeout(() => {
+        this.setState({ isOpen: false });
+      }, 200);
+    }
+  };
+
+  handleBranch = e => {
+    e.target.focus();
+    this.setState({ isOpen: !this.state.isOpen });
+  };
+
   render() {
     return (
-      <div id="repo-features">
-        <div>
-          <button className="white-button">
-            Master
-            <img id="leftfeature-image" src={arrow_down_blue_01} alt="" />
-          </button>
+      <>
+        <div id="repo-features">
+          <div>
+            <div className="reference">
+              <button className="white-button" onClick={this.handleBranch}>
+                {this.state.branchSelected}
+                <img id="leftfeature-image" src={arrow_down_blue_01} alt="" />
+              </button>
+              {this.state.isOpen && (
+                <div className="select-branch" onBlur={this.handleBlur}>
+                  <div
+                    style={{
+                      margin: "0 50px",
+                      fontSize: "14px",
+                      padding: "0 40px"
+                    }}
+                  >
+                    <p>Switch Branches</p>
+                  </div>
+                  <hr />
+                  <div className="search-branch">
+                    <input
+                      autoFocus={true}
+                      type="text"
+                      placeholder="Search branches or tags"
+                    />
+                    <div className="branches">
+                      <ul>
+                        <li className="branch-header">Branches</li>
+                        {this.Branches.map((branch, index) => (
+                          <li key={index}>
+                            <p>{branch}</p>
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              )}
+            </div>
+            <button className="white-button">
+              <img id="plus" src={plus_01} alt="" />
+              <img id="leftfeature-image" src={arrow_down_blue_01} alt="" />
+            </button>
 
-          <button className="white-button">
-            <img id="plus" src={plus_01} alt="" />
-            <img id="leftfeature-image" src={arrow_down_blue_01} alt="" />
-          </button>
+            <button className="blue-button">
+              <p>Data Visualisation</p>
+            </button>
 
-          <button className="blue-button">
-            <p>Data Visualisation</p>
-          </button>
+            <button className="blue-button">
+              <p>Data Pipeline</p>
+            </button>
+          </div>
+          <div>
+            <button className="white-button">
+              <p>History</p>
+            </button>
 
-          <button className="blue-button">
-            <p>Data Pipeline</p>
-          </button>
+            <button className="white-button">
+              <p>Web IDE</p>
+            </button>
+
+            <button className="white-button">
+              <img id="leftfeature-image" src={arrow_down_blue_01} alt="" />
+            </button>
+          </div>
         </div>
-        <div>
-          <button className="white-button">
-            <p>History</p>
-          </button>
-
-          <button className="white-button">
-            <p>Web IDE</p>
-          </button>
-
-          <button className="white-button">
-            <img id="leftfeature-image" src={arrow_down_blue_01} alt="" />
-          </button>
-        </div>
-      </div>
+      </>
     );
   }
 }
