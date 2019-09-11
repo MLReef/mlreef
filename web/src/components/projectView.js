ad6a4561eba4a44feef640a3fc1d4ba90d69d3b9
SaathvikT <saathvik1997@gmail.com>
2019-09-11 22:42:15 +0000

    Feature/raw readme

--- web/src/components/projectView.js
+++ web/src/components/projectView.js
@@ -1,77 +1,77 @@
 import React from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import ReadMeComponent from './../components/readMe'
 import ProjectContainer from './projectContainer';
 import FilesContainer from './files-container';
 import RepoInfo from './repo-info';
 import RepoFeatures from './repo-features';
 import Navbar from './navbar/navbar';
-import {bindActionCreators} from 'redux';
+import { bindActionCreators } from 'redux';
 import * as fileActions from "./../actions/fileActions";
 import "../css/index.css";
 
-class ProjectView extends React.Component{
+class ProjectView extends React.Component {
     state = {
         project: null
     }
 
-    componentWillMount(){
+    componentWillMount() {
         const project = this.props.projects.filter(proj => proj.id === parseInt(this.props.match.params.projectId))[0];
-        this.setState({project: project});
+        this.setState({ project: project });
         this.props.actions.loadFiles(
-            null, 
+            null,
             project.default_branch,
             this.props.match.params.projectId
         );
     }
 
-    componentWillUpdate(){
+    componentWillUpdate() {
         const projectId = window.location.href.split("/my-projects/")[1]
-        
-        if(this.state.project.id !== parseInt(projectId)){
+
+        if (this.state.project.id !== parseInt(projectId)) {
             const project = this.props.projects.filter(proj => proj.id === parseInt(projectId))[0];
             this.setState({
                 project: project
             });
             this.props.actions.loadFiles(
-                null, 
+                null,
                 project.default_branch,
                 projectId
             );
-        }   
+        }
     }
-    
+
     render() {
         const files = this.props.files;
         const branch = this.state.project.default_branch;
         const projectName = this.state.project.name;
         return (<div className="project-component">
-            <Navbar/>
-            <ProjectContainer project={this.state.project} activeFeature="data" folders={["Group Name", projectName, "Data"]}/>
+            <Navbar />
+            <ProjectContainer project={this.state.project} activeFeature="data" folders={["Group Name", projectName, "Data"]} />
             <div className="main-content">
-                <RepoInfo/>
+                <RepoInfo />
                 <div className="last-commit-info">
-                        <div className="last-commit-details">
-                            <div className="last-commit-pic"></div>
-                            <div className="last-commit-name">
-                                Merged Branch <b>"branch_name"</b> into <b>"new_branch_name"</b><br/>
-                                by <b>user_name</b> authored <b>4_days_ago</b>
-                            </div>
+                    <div className="last-commit-details">
+                        <div className="last-commit-pic"></div>
+                        <div className="last-commit-name">
+                            Merged Branch <b>"branch_name"</b> into <b>"new_branch_name"</b><br />
+                            by <b>user_name</b> authored <b>4_days_ago</b>
                         </div>
+                    </div>
 
                     <div className="last-commit-id">
-                            <p>PR_ID</p>
-                        </div>              
+                        <p>PR_ID</p>
+                    </div>
                 </div>
-                <RepoFeatures/>
-                <FilesContainer projectId={this.state.project.id} branch={branch} files={files}/>
-                <ReadMeComponent/>
+                <RepoFeatures />
+                <FilesContainer projectId={this.state.project.id} branch={branch} files={files} />
+                <ReadMeComponent projectId={this.state.project.id} branch={branch} files={files} />
             </div>
         </div>)
     }
 }
 
-function mapStateToProps(state){
+function mapStateToProps(state) {
     return {
         files: state.files,
         projects: state.projects,
@@ -79,7 +79,7 @@ function mapStateToProps(state){
     };
 }
 
-function mapDispatchToProps(dispatch){
+function mapDispatchToProps(dispatch) {
     return {
         actions: bindActionCreators(fileActions, dispatch)
     };
--- web/src/components/readMe.js
+++ web/src/components/readMe.js
@@ -1,21 +1,70 @@
 import React from 'react';
+import { connect } from "react-redux";
+import * as fileActions from "../actions/fileActions";
+import { Base64 } from "js-base64";
+import { bindActionCreators } from "redux";
 
-export default class ReadMeComponent extends React.Component{
-    render(){
-        return <div className="readme-container">  
+class ReadMeComponent extends React.Component {
+
+    componentWillMount() {
+        const projectId = this.props.projectId;
+        const proj = this.props.projects.filter(proj => proj.id === parseInt(projectId))[0];
+        this.setState({ project: proj })
+        this.props.actions.getFileData("gitlab.com", projectId, "README.md", this.props.branch);
+    }
+
+    render() {
+        const projectName = this.state.project.name;
+        let fileContent = [];
+        if (this.props.fileData.content) {
+            fileContent = Base64.decode(this.props.fileData.content).split("\n");
+        }
+        return <div className="readme-container">
             <div className="readme-titlebar">
                 <div className="readme-profile-pic"></div>
                 <div className="readme-name">
                     <p id="readmeName"><b>README.md</b></p>
                 </div>
-            </div>  
+            </div>
 
             <div className="readme-content-container">
                 <div className="readme-content">
-                    <p id="project-name-readme">Project Name</p>
-                    <p id="project-content-readme">Content of the README.md file is displayed here ...</p>
+                    <p id="project-name-readme">{projectName}</p>
+                    <div id="project-content-readme">
+                        <table>
+                            <tbody>
+                                {fileContent.map(function (line, index) {
+                                    return (
+                                        <tr key={index}>
+                                            <td>
+                                                <p>{line}</p>
+                                            </td>
+                                        </tr>
+                                    );
+                                })}
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
-            </div>      
+            </div>
         </div>
     }
 }
+
+function mapStateToProps(state) {
+    return {
+        fileData: state.file,
+        projects: state.projects
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        actions: bindActionCreators(fileActions, dispatch)
+    };
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(ReadMeComponent);
