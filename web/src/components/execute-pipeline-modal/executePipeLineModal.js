2015ba2e6de9fd2b1022acae3abce23cbdad448b
Andres Ausecha <andres@aamsis.co>
2019-10-09 12:50:32 +0000

    Feature/120 process training modal

--- web/src/components/execute-pipeline-modal/executePipeLineModal.js
+++ web/src/components/execute-pipeline-modal/executePipeLineModal.js
@@ -36,7 +36,7 @@ const ExecutePipelineModal = ({
     return isShowing
     ? (
         <div className="generic-modal">
-            <div className="modal-content" style={{height: '30%', minHeight: 250}}>
+            <div className="modal-content" style={{height: '50%', minHeight: 250}}>
                 <div className="title light-green-button">
                     <div style={{padding: '0 3em'}}>
                         <p>
@@ -51,81 +51,96 @@ const ExecutePipelineModal = ({
                     </div>
                 </div>
                 {section === 1 && <>
-                    <div style={{padding: '1em 0 1em 3em', cursor: 'pointer', height: '50%' }}>
+                    <div style={{ padding: '1em 0 1em 3em', cursor: 'pointer', height: '60%' }}>
                         <div 
                             style={{display: 'flex', alignItems: 'center'}} 
                             onClick={ () => {
-                                const secondOptExecModal = document.getElementById("second-option-execution-modal");
-                                if(secondOptExecModal){
-                                    secondOptExecModal.style.marginTop = !isFirstOptSelected ? '3em': '0';
-                                }
                                 setIsFirstOptSelected(!isFirstOptSelected);
                                 setIsSecondOptSelected(false);
+                                document.getElementById("paragraph-op1").style.fontWeight = 700;
+                                document.getElementById("paragraph-op2").style.fontWeight = 100;
                             }}
                         >
                             <input type="radio" checked={isFirstOptSelected} id="show-first-opt" onChange={() => {}}/>
-                            <p style={{marginLeft: "1em", fontWeight: "700"}}>Create a new set of data instances in your data repository</p>
+                            <p style={{marginLeft: "1em"}} id="paragraph-op1" >Create a new set of data instances in your data repository</p>
                         </div>
-                        {isFirstOptSelected &&
-                            <div style={{width: '50%', borderRadius: '0.5em', marginLeft: '2.5em', position: 'absolute', zIndex: '2', backgroundColor: 'white'}}
-                                className="drop-down-select blue-border-on-hover"
-                            >
-                                <div style={{display: 'flex', alignItems: 'center'}} 
-                                    onClick={() => {
-                                        $(`#machines-drop-down-btn`).click();
-                                    }}
+                        {isFirstOptSelected && 
+                            <>
+                                <div style={{width: '50%', borderRadius: '0.5em', marginLeft: '2.5em', position: 'absolute', zIndex: '2', backgroundColor: 'white'}}
+                                    className="drop-down-select blue-border-on-hover"
                                 >
-                                    <div style={{width: '90%'}}>
-                                        <p className="machines-paragraph">{selectMachinesMess}</p>
+                                    <div style={{display: 'flex', alignItems: 'center'}} 
+                                        onClick={() => {
+                                            $(`#machines-drop-down-btn`).click();
+                                        }}
+                                    >
+                                        <div style={{width: '90%'}}>
+                                            <p className="machines-paragraph">{selectMachinesMess}</p>
+                                        </div>
+                                        <div style={{width: '10%', display: 'flex', justifyContent: 'flex-end'}}>
+                                            <ArrowButton 
+                                                imgPlaceHolder={traiangle01}
+                                                callback={() => {
+                                                    setShowMachines(!showMachines);
+                                                }}
+                                                params={{}}
+                                                id={'machines-drop-down-btn'}
+                                            />
+                                        </div>
                                     </div>
-                                    <div style={{width: '10%', display: 'flex', justifyContent: 'flex-end'}}>
-                                        <ArrowButton 
-                                            imgPlaceHolder={traiangle01}
-                                            callback={() => {
-                                                setShowMachines(!showMachines);
-                                            }}
-                                            params={{}}
-                                            id={'machines-drop-down-btn'}
-                                        />
+                                    {showMachines && 
+                                        <ul style={{margin: 0, padding: 0, listStyleType: 'none'}} id="machines-list">
+                                            {fakeMachinesToShow.map((machine, index) =>
+                                                <li key={`machine-${index}`} id={`machine-${index}`} onClick={(e) => {
+                                                    $(`#machines-drop-down-btn`).click();
+                                                    setSelectMachinesMess($.parseHTML(machine)[0].data);
+                                                }}>
+                                                    <p className="blue-background-on-hover machines-paragraph">
+                                                        {machine}
+                                                    </p>
+                                                </li>
+                                            )}
+                                        </ul>
+                                    }
+                                </div>
+                                <p style={{fontSize: '0.8em', margin: '5em 0em 0em 3em'}}>Select a hyperparameter tuning method</p>
+                                <div style={{display: 'flex', marginLeft: '1.8em'}}>
+                                    <div style={{display: 'inherit'}}>
+                                        <input id="execution-radio-input-1" type="radio" onChange={() => {}}/>
+                                        <p onClick={() => { $("#execution-radio-input-1").click()}}>Simple execution</p>
+                                    </div>
+                                    <div style={{display: 'inherit'}}>
+                                        <input id="execution-radio-input-2" type="radio" onChange={() => {}}/>
+                                        <p onClick={() => { $("#execution-radio-input-2").click()}}>Grid search</p>
+                                    </div>
+                                    <div style={{display: 'inherit'}}>
+                                        <input id="execution-radio-input-3" type="radio" onChange={() => {}}/>
+                                        <p onClick={() => { $("#execution-radio-input-3").click()}}>Bayesian</p>
                                     </div>
                                 </div>
-                                {showMachines && 
-                                    <ul style={{margin: 0, padding: 0, listStyleType: 'none'}} id="machines-list">
-                                        {fakeMachinesToShow.map((machine, index) =>
-                                            <li key={`machine-${index}`} id={`machine-${index}`} onClick={(e) => {
-                                                $(`#machines-drop-down-btn`).click();
-                                                setSelectMachinesMess($.parseHTML(machine)[0].data);
-                                            }}>
-                                                <p className="blue-background-on-hover machines-paragraph">
-                                                    {machine}
-                                                </p>
-                                            </li>
-                                        )}
-                                    </ul>
-                                }
-                            </div>
+                            </>
                         }
                         <div id="second-option-execution-modal"
                             style={{display: 'flex', alignItems: 'center', cursor: 'pointer', marginTop: 0}} 
-                            onClick={ () => {
+                            onClick={ (e) => {
                                 const secondOptExecModal = document.getElementById("second-option-execution-modal");
                                 if(secondOptExecModal){
                                     secondOptExecModal.style.marginTop = '0';
                                 }
                                 setIsFirstOptSelected(false);
                                 setIsSecondOptSelected(!isSecondOptSelected);
+                                document.getElementById("paragraph-op1").style.fontWeight = 100;
+                                document.getElementById("paragraph-op2").style.fontWeight = 700;
                             }}
                         >
                             <input type="radio" checked={isSecondOptSelected} onChange={() => {}}/>
-                            <p style={{marginLeft: "1em", fontWeight: "700"}}>
+                            <p style={{marginLeft: "1em"}} id="paragraph-op2">
                                 Launch Jupyter notebook on your web browser to execute the pipeline locally on your machine
                             </p>
                         </div>
-                        
-                    </div>
-                    
+                    </div>    
                 </>}
-                {section === 2 && <div style={{padding: '1em 0 1em 3em', cursor: 'pointer', height: '50%' }}>
+                {section === 2 && <div style={{padding: '1em 0 1em 3em', cursor: 'pointer', height: '70%' }}>
                     <p>Data instance: <b>DI_ProjectName_1</b></p>
                     <div style={{
                             backgroundColor: 'white',
@@ -165,7 +180,7 @@ const ExecutePipelineModal = ({
                                 onClick={() => {
                                     if(section === 1){
                                         setSection(2);
-                                        handleExecuteModalBtnNextPressed();
+                                        /* handleExecuteModalBtnNextPressed(); */
                                     } else {
                                         cleanForm();
                                     }
@@ -178,7 +193,7 @@ const ExecutePipelineModal = ({
                                     }
                             </button>
                         </div>
-                    </div>
+                </div>
             </div>
         </div>
     )
