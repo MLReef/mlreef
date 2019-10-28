8acaf69220a59229dc5c04468eb14d92606bc8ba
Florian Hintermeier <florian@hintermeier.at>
2019-10-28 14:32:27 +0000

    Feature #11: Dockerize backend with Dockerfile and docker-compose
    - add Dockerfile for spring-boot container
    - add gradle tasks to explode jar in refresh docker

--- mlreef-rest/src/main/kotlin/com/mlreef/rest/config/application_config.kt
+++ mlreef-rest/src/main/kotlin/com/mlreef/rest/config/application_config.kt
@@ -1,9 +1,13 @@
 package com.mlreef.rest.config
 
-import java.util.ArrayList
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.cache.CacheManager
+import org.springframework.cache.annotation.EnableCaching
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import java.util.*
 
 
 @Configuration
@@ -17,4 +21,14 @@ open class YAMLConfig {
 
     // standard getters and setters
 
+}
+
+@Configuration
+@EnableCaching
+open class CachingConfig {
+
+    @Bean
+    open fun cacheManager(): CacheManager {
+        return ConcurrentMapCacheManager("addresses")
+    }
 }
--- mlreef-rest/src/main/kotlin/com/mlreef/rest/config/security_config.kt
+++ mlreef-rest/src/main/kotlin/com/mlreef/rest/config/security_config.kt
@@ -1,6 +1,6 @@
 package com.mlreef.rest.config
 
-import com.mlreef.rest.external_api.gitlab.GitlabUserDetails
+import com.mlreef.rest.external_api.gitlab.TokenDetails
 import lombok.extern.slf4j.Slf4j
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
@@ -18,6 +18,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.core.Authentication
+import org.springframework.security.core.AuthenticationException
+import org.springframework.security.core.context.SecurityContext
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.web.AuthenticationEntryPoint
@@ -25,18 +27,29 @@ import org.springframework.security.web.authentication.AbstractAuthenticationPro
 import org.springframework.security.web.authentication.AnonymousAuthenticationFilter
 import org.springframework.security.web.authentication.HttpStatusEntryPoint
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher
 import org.springframework.security.web.util.matcher.OrRequestMatcher
 import org.springframework.security.web.util.matcher.RequestMatcher
 import org.springframework.session.FindByIndexNameSessionRepository
 import org.springframework.session.Session
 import org.springframework.stereotype.Component
+import java.util.logging.Logger
 import javax.servlet.FilterChain
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
+import javax.servlet.http.HttpSession
 
+fun String.censor(): String {
+    return if (this.length > 2) {
+        val half = this.length / 2
+        this.replaceRange(half, this.length - 1, "*")
+    } else {
+        ""
+    }
+}
 interface AuthService {
-    fun findByToken(token: String): GitlabUserDetails?
+    fun findByToken(token: String): TokenDetails?
 }
 
 @Slf4j
@@ -48,19 +61,43 @@ open class CustomAuthenticationProvider : AbstractUserDetailsAuthenticationProvi
     @Autowired
     lateinit var sessionRepository: FindByIndexNameSessionRepository<out Session>
 
-    override fun retrieveUser(username: String?, authentication: UsernamePasswordAuthenticationToken?): UserDetails {
-        if (username == null) {
-            throw BadCredentialsException("username is null during AuthenticationProvider")
+    override fun retrieveUser(token: String?, authentication: UsernamePasswordAuthenticationToken?): UserDetails {
+        if (token == null) {
+            throw BadCredentialsException("token is null during AuthenticationProvider")
         }
         if (authentication == null) {
             throw BadCredentialsException("authentication is null during AuthenticationProvider")
+        }
+
+        val fromSession: UserDetails? = retrieveFromSession(token)
+        if (fromSession != null) {
+            return fromSession
+        }
+        val fromCache: UserDetails? = this.userCache.getUserFromCache(token)
+        if (fromCache != null) {
+            return fromCache
         }
         val findByToken = authService.findByToken(authentication.name)
         if (findByToken == null) {
             throw BadCredentialsException("Token not found in Gitlab!")
         } else {
+            // store user in cache! This method will not be called if cache can be used!
+            this.userCache.putUserInCache(findByToken)
             return findByToken
         }
+    }
+
+    private fun retrieveFromSession(token: String): UserDetails? {
+        val findByPrincipalNameMap = sessionRepository.findByIndexNameAndIndexValue(
+                FindByIndexNameSessionRepository.PRINCIPAL_NAME_INDEX_NAME, token)
+
+        if (findByPrincipalNameMap.isNotEmpty()) {
+            logger.info("Reuse session for token:${token.censor()}")
+            val first = findByPrincipalNameMap.values.first()
+            return first.getAttribute<UserDetails>("user")
+        }
+
+        return null
     }
 
     override fun additionalAuthenticationChecks(userDetails: UserDetails?, authentication: UsernamePasswordAuthenticationToken?) {
@@ -69,32 +106,53 @@ open class CustomAuthenticationProvider : AbstractUserDetailsAuthenticationProvi
 
 @Slf4j
 class CustomAuthenticationFilter(requestMatcher: RequestMatcher) : AbstractAuthenticationProcessingFilter(requestMatcher) {
+    @Throws(AuthenticationException::class)
     override fun attemptAuthentication(request: HttpServletRequest?, response: HttpServletResponse?): Authentication {
         val tokenHeader = request?.getHeader("PRIVATE-TOKEN")
         val token = tokenHeader?.removePrefix("Bearer")?.trim()
         val springToken = UsernamePasswordAuthenticationToken(token, token)
-        return authenticationManager.authenticate(springToken)
+        val authenticate = authenticationManager.authenticate(springToken)
+
+        val securityContext: SecurityContext = SecurityContextHolder.getContext()
+        securityContext.authentication = authenticate
+        val session: HttpSession? = request?.getSession(true)
+        session?.setAttribute(SPRING_SECURITY_CONTEXT_KEY, securityContext)
+
+        if (!authenticate.isAuthenticated) {
+            throw BadCredentialsException("Not authenticated")
+        }
+        return authenticate
     }
 
     override fun successfulAuthentication(request: HttpServletRequest?, response: HttpServletResponse?, chain: FilterChain?, authResult: Authentication?) {
         SecurityContextHolder.getContext().authentication = authResult
         request?.session?.setAttribute("isAuthenticated", authResult!!.isAuthenticated)
-        chain?.doFilter(request, response)
+//        chain?.doFilter(request, response)
+    }
+
+    override fun unsuccessfulAuthentication(request: HttpServletRequest?, response: HttpServletResponse?, failed: AuthenticationException?) {
+        super.unsuccessfulAuthentication(request, response, failed)
+        request?.session?.setAttribute("isAuthenticated", false)
+//        chain?.doFilter(request, response)
     }
 }
 
 @Slf4j
 class CustomSessionStrategy<T : Session>(private val sessionRepo: FindByIndexNameSessionRepository<T>) : SessionAuthenticationStrategy {
 
+    val log = Logger.getLogger(this::class.simpleName)
+
     override fun onAuthentication(authentication: Authentication?, request: HttpServletRequest?, response: HttpServletResponse?) {
 
-        val userDetails = authentication?.principal as GitlabUserDetails
+        val userDetails = authentication?.principal as TokenDetails
         val token = userDetails.token
 
         val findByPrincipalNameMap = sessionRepo.findByIndexNameAndIndexValue(FindByIndexNameSessionRepository.PRINCIPAL_NAME_INDEX_NAME, token)
         val session: T = if (findByPrincipalNameMap.isNotEmpty()) {
+            log.info("Reuse session for token:${token.censor()}")
             findByPrincipalNameMap.values.first()
         } else {
+            log.info("Create session for token:${token.censor()}")
             sessionRepo.createSession()
         }
         session.setAttribute(FindByIndexNameSessionRepository.PRINCIPAL_NAME_INDEX_NAME, token)
@@ -122,15 +180,13 @@ open class SecurityConfiguration(var provider: AuthenticationProvider) : WebSecu
     @Throws(Exception::class)
     public override fun configure(http: HttpSecurity) {
         http.sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
                 .and()
                 .exceptionHandling()
                 .and()
                 .authenticationProvider(provider)
                 .addFilterBefore(authenticationFilter(), AnonymousAuthenticationFilter::class.java)
-                .authorizeRequests()
-                .requestMatchers(PROTECTED_URLS)
-                .authenticated()
+                .authorizeRequests().requestMatchers(PROTECTED_URLS).authenticated()
                 .and()
                 .csrf().disable()
                 .formLogin().disable()
