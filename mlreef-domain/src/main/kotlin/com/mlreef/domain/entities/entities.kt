60ced1fa485443d10224a03d6b414bdfabb1d97c
Florian Hintermeier <florian@hintermeier.at>
2019-11-05 13:26:25 +0000

    Feature #10: Add first draft of data models from mlreef domain perspective
    - add plantUml for mlreef domain model
    - add first enums and entities in Kotlin

--- mlreef-domain/src/main/kotlin/com/mlreef/domain/entities/entities.kt
+++ mlreef-domain/src/main/kotlin/com/mlreef/domain/entities/entities.kt
@@ -1,78 +1,114 @@
 package com.mlreef.domain.entities
 
+import java.io.File
 import java.util.*
-import javax.persistence.Embeddable
+import javax.persistence.*
 
+@MappedSuperclass
+abstract class BaseEntity {
+    abstract val id: UUID?
+    val createdAt: Long = 0
+    val updatedAt: Long = 0
+}
 
-open class Subject {
-    val id: UUID? = null
+@MappedSuperclass
+abstract class Subject : BaseEntity() {
     val slug: String = ""
     val name: String = ""
 }
 
-class Person : Subject() {
-    val memberships: List<Organisation> = listOf()
-}
+@Entity
+class Person(
+        @Id @GeneratedValue override val id: UUID? = null,
+        @ManyToMany val memberships: List<Organisation> = listOf()
+) : Subject()
 
-class Organisation : Subject() {
-    val members: List<Person> = listOf()
-}
+@Entity
+class Organisation(
+        @Id @GeneratedValue override val id: UUID? = null,
+        @ManyToMany val members: List<Person> = listOf()
+) : Subject()
 
 enum class VisibilityScope {
     PRIVATE,
     PUBLIC,
-    TEAM_PRIVATE
+    TEAM
 }
 
-/**
- * A Taggable entity can be described and linked with certain Tags
- *
- */
-interface Taggable {
-    val tags: List<Tag>
-        get() = listOf()
-}
 
-class Tag {
-    val name: String = ""
-}
 
 /**
  * Symbolize a Gitlab repository which is used in the context of MLReef.
  * Most properties are transient, semantic will be added
  */
-open class MLRepository : Taggable {
-    val id: UUID? = null
-    val slug: String = ""
-    val url: String = ""
-    val owner: Subject? = null
-}
+@MappedSuperclass
+abstract class MLRepository(
+        @Id @GeneratedValue override val id: UUID? = null,
+        val slug: String,
+        val url: String,
+        val owner: Subject
+) : BaseEntity()
 
 /**
  * A Machine Learning Repository describes the association of data and experiments.
- * A repo can also be described with an DataType, for example a MLDataRepository using Images or signal streams as data
+ * A repo can also be described with an DataType, for example a MLDataRepository using Images a data set
  */
-class MLDataRepository : MLRepository() {
-    val experiments: List<Experiment> = listOf()
-    val dataTypes: List<DataType> = listOf()
-}
+@Entity
+class MLDataRepository(
+        @Id @GeneratedValue override val id: UUID? = null,
+        slug: String,
+        url: String,
+        owner: Subject,
+        val experiments: List<Experiment> = listOf(),
+        val dataTypes: List<DataType> = listOf()
+) : MLRepository(id, slug, url, owner)
 
 /**
  * A Code Repository is used for the working Code like Data Operations,
  * Algorithms, or soon Visualisations.
  *
+ * A
+ */
+@Entity
+open class MLCodeRepository(
+        @Id @GeneratedValue override val id: UUID? = null,
+        slug: String,
+        url: String,
+        owner: Subject,
+        val dataOperation: DataOperation? = null
+) : MLRepository(id, slug, url, owner)
+
+/**
+ * Descriptor of an Output, which could be a Gitlab Artifact, an S3 Bucket or many more.
+ *
  *
  */
-open class MLCodeRepository : MLRepository() {
-    val dataOperation: DataOperation? = null
-}
 
+@Entity
+class OutputFile(override val id: UUID?) : BaseEntity()
 
-class Experiment {
-    val id: UUID? = null
-    val performanceMetrics: PerformanceMetrics? = null
-    val createdAt: Long = 0
-}
+/**
+ * An Experiment is a instance of a ProcessingChain with Data
+ */
+@Entity
+class Experiment(
+        @Id @GeneratedValue override val id: UUID? = null,
+        val performanceMetrics: PerformanceMetrics? = null,
+        val outputFile: List<OutputFile>? = null,
+
+        /**
+         * Has DataOps and DataVisuals
+         */
+        val preProcessing: List<DataProcessorInstance>? = null,
+        /**
+         * Has DataOps and maybe DataVisuals
+         */
+        val postProcessing: List<DataProcessorInstance>? = null,
+        /**
+         * Contains exactly 1 Algorithm (could be implement as DataExecutionInstance as well)
+         */
+        val processing: DataProcessorInstance? = null
+) : BaseEntity()
 
 @Embeddable
 class PerformanceMetrics {
@@ -83,24 +119,54 @@ class PerformanceMetrics {
 }
 
 /**
- * A DataOperation can be applied onto Data to filter or manipulate the Data.
- * The result of DataOperation over Data is Data again.
+ * A DataProcessor can be applied onto Data to filter or manipulate the Data.
+ * The result of DataOperation over Data is Data again, or in special cases, Visualisation output
  *
  * The source and input DataTypes must be defined to be usable for the community
  * The List of Parameters is ordered and names must be uniqe.
+ *
+ * DataOperations can be chained in a execution
  */
-class DataOperation : Taggable {
-    val repository: MLCodeRepository? = null
-    val parameters: List<DOParameter> = listOf()
-    val resultDataType: DataType? = null
-    val inputDataType: DataType? = null
-    val id: UUID? = null
-    val slug: String = ""
-    val name: String = ""
-    val author: Subject? = null
-    val description: String = ""
-    val visibilityScope: VisibilityScope? = null
+open class DataProcessor(
+        @Id @GeneratedValue override val id: UUID? = null,
+        val repository: MLCodeRepository? = null,
+        val parameters: List<DOParameter> = listOf(),
+        val inputDataType: DataType? = null,
+        val slug: String = "",
+        val name: String = "",
+        val author: Subject? = null,
+        val description: String = "",
+        val visibilityScope: VisibilityScope? = null
+) : BaseEntity() {
+
 }
+
+/**
+ * DataOperation perform on Data and create Data.
+ * Therefore they must be chainable
+ */
+class DataOperation : DataProcessor() {
+    val resultDataType: DataType? = null
+}
+
+class DataVisualization : DataProcessor() {
+    val resultFiles: List<File> = listOf()
+}
+
+/**
+ * Proposal: Model DataAlgorithm as a Data processor, even if it not chainable
+ */
+class DataAlgorithm : DataProcessor() {
+}
+
+/**
+ * An Instance of DataOperation or DataVisualisation contains instantiated values of Parameters
+ */
+interface DataProcessorInstance {
+    val parent: DataProcessorInstance?
+    val parameterValues: List<ParameterInstance>
+}
+
 
 /**
  * A Parameter describes a native data type (e.g. floating number in python), with a title and descriptions.
@@ -114,6 +180,10 @@ class DOParameter {
     val defaultValue: String = ""
 }
 
+class ParameterInstance {
+    val parameter: DOParameter? = null
+    val value: Any? = null
+}
 /**
  * ParameterTypes are typical simple data types.
  * Lists and Objects should be supported, but cannot be interfered in a global scope.
@@ -124,13 +194,13 @@ enum class ParameterType {
     BOOLEAN,
     STRING,
     INTEGER,
-    LONG,
     COMPLEX,
     FLOAT,
     LIST,
     TUPLE,
     DICTIONARY,
-    OBJECT
+    OBJECT,
+    NUMPY_ARRAY
 }
 
 /**
@@ -145,9 +215,11 @@ enum class ParameterType {
 enum class DataType {
     ANY,
     IMAGE,
+    VIDEO,
     SENSOR,
     NUMBER,
-    MATRIX,
+    TABULAR,
     TEXT,
-    BINARY
+    BINARY,
+    MODEL
 }
