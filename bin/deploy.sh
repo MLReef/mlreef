91869690f01fc6f7e94010c1001a65f45a966b39
Rainer Kern <rainer@systemkern.com>
2019-11-22 16:35:02 +0000

    Add branch instance backup
    Add gitlab job to create an s3 backup of
    the current instance.
    Add gitlab job to restore s3 backups of
    the current branch and from develop

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,38 +2,63 @@
 set -e
 set -x
 
+PORT=10080
 
-if [ "$1" = "" ]; then
-  echo "Missing instance url"
+alias remote-ec2="ssh -i development_deployment.pem -o 'AddKeysToAgent yes' ubuntu@\${INSTANCE}"
+
+while [ -n "$1" ]; do
+  case "$1" in
+  -f | --file) IMAGE_PATH="$2"
+    echo "Using docker image: $IMAGE_PATH"
+    shift ;;
+  -i | --instance) INSTANCE="$2"
+    echo "Connecting to ec2 instance $INSTANCE"
+    shift ;;
+  -p | --port) PORT="$2"
+    echo "Expecting gitlab at port $PORT"
+    shift ;;
+  *) echo "Option $1 not recognized" ;;
+  esac
+  shift
+done
+
+echo "Successfuly parsed parameters"
+
+if [ "$INSTANCE" = "" ]; then
+  echo "Missing instance url. Use the -i or --instance option"
+  exit 1
+fi
+if [ "$IMAGE_PATH" = "" ]; then
+  echo "Missing docker image. Use the -I or --image option"
   exit 1
 fi
 
-if [ "$2" = "" ]; then
-  echo "Missing IMAGE_PATH"
-  exit 1
-fi
-INSTANCE=$1
-IMAGE_PATH=$2
 
 echo "starting deploy.sh"
 echo "----------------------------------------"
-echo "The EC2 instance is: $INSTANCE"
-echo "Frontend image path IMAGE_PATH is $IMAGE_PATH"
 
-ssh-keyscan -H "$INSTANCE" >> ~/.ssh/known_hosts
+ssh-keyscan -H "$INSTANCE" >>~/.ssh/known_hosts
 
-alias remote-ec2="ssh -i development_deployment.pem -o 'AddKeysToAgent yes' ubuntu@\${INSTANCE} "
+DOCKER_COMPOSE="docker-compose.yml"
 
 # rewrite docker dompose file
-sed -i "s~master/bin~$CI_COMMIT_REF_NAME/bin~"                    docker-compose.yml
-sed -i "s/$IMAGE_NAME:master/$IMAGE_NAME:$CI_COMMIT_REF_SLUG/"    docker-compose.yml
-sed -i "s~-\ postgresql-data~-\ /data/postgres~"                  docker-compose.yml
-sed -i "s~-\ gitlab-data~-\ /data/gitlab~"                        docker-compose.yml
-sed -i "s~GITLAB_HOST=localhost~GITLAB_HOST=$INSTANCE~"           docker-compose.yml
-sed -i "s/\ \ postgresql-data/#postgresql-data/"                  docker-compose.yml
-sed -i "s/\ \ gitlab-data/#postgresql-data/"                      docker-compose.yml
-cat docker-compose.yml
-scp -i development_deployment.pem -o 'AddKeysToAgent yes' docker-compose.yml ubuntu@${INSTANCE}:~
+# change download path from master branch to current branch on gitlab.com
+#sed -i "s~master/bin~$CI_COMMIT_REF_NAME/bin~" docker-compose.yml
+# change the frontend docker image from master branch to current branch
+sed -i "s/$IMAGE_NAME:master/$IMAGE_NAME:$CI_COMMIT_REF_SLUG/"          $DOCKER_COMPOSE
+# change postgres-data local image to /data/postgres on host machine
+sed -i "s~-\ postgresql-data~-\ /data/postgres~"                        $DOCKER_COMPOSE
+# change gitlab-data local image to /data/gitlab on host machine
+sed -i "s~-\ gitlab-data~-\ /data/gitlab~"                              $DOCKER_COMPOSE
+# Let gitlab know its own URL
+# deactivate "docker portable volumes
+sed -i "s/\ \ postgresql-data/#postgresql-data/"                        $DOCKER_COMPOSE
+sed -i "s/\ \ gitlab-data/#postgresql-data/"                            $DOCKER_COMPOSE
+sed -i "s~GITLAB_HOST=localhost~GITLAB_HOST=$INSTANCE~"                 $DOCKER_COMPOSE
+cat $DOCKER_COMPOSE
+
+# upload modified compose to ec2 instance
+scp -i development_deployment.pem -o 'AddKeysToAgent yes' $DOCKER_COMPOSE ubuntu@${INSTANCE}:
 
 remote-ec2 "sudo docker-compose up -d"
 
@@ -51,5 +76,3 @@ remote-ec2 "sudo docker exec -i gitlab-runner gitlab-ci-multi-runner register  \
   --tag-list \"docker,aws\"                     \
   --run-untagged=true                           \
   --locked=\"false\"                            "
-
-
