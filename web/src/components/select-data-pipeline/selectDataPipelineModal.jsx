fda160739f36006b724f10a7c46ab71f5e9550e9
AndresAusecha <daausecham@unal.edu.co>
2019-11-28 10:19:01 -0500

    include material UI to improve styles

--- web/src/components/select-data-pipeline/selectDataPipelineModal.jsx
+++ web/src/components/select-data-pipeline/selectDataPipelineModal.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Checkbox from '@material-ui/core/Checkbox';
 import '../../css/genericModal.css';
 import './selectDataPipelineModal.css';
 import Input from '../input/input';
@@ -28,12 +29,12 @@ class SelectDataPipelineModal extends Component {
       show: nextProps.show,
     })
 
-    handleCloseButton(e) {
+    handleCloseButton() {
       this.props.selectDataClick();
       document.getElementsByTagName('body').item(0).style.overflow = 'scroll';
     }
 
-    handleBranch = (e, params) => {
+    handleBranch = (e) => {
       e.target.focus();
       this.setState({ isOpen: !this.state.isOpen });
     }
@@ -51,29 +52,40 @@ class SelectDataPipelineModal extends Component {
       this.setState({ filesSelected });
     }
 
-    getFiles = (newBranchSelectedName) => filesApi
-      .getFilesPerProject(
-        this.state.project.id,
-        '',
-        false,
-        newBranchSelectedName,
-      )
-      .then(
-        (res) => this.setState({
-          filesSelected: [],
-          branchSelected: newBranchSelectedName,
-          files: res,
-        }),
-      )
-      .catch(
-        (err) => err,
-      )
+    getFiles = (newBranchSelectedName) => {
+      const { project: { id } } = this.state;
+      filesApi
+        .getFilesPerProject(
+          id,
+          '',
+          false,
+          newBranchSelectedName,
+        )
+        .then(
+          (res) => this.setState({
+            filesSelected: [],
+            branchSelected: newBranchSelectedName,
+            files: res,
+          }),
+        )
+        .catch(
+          (err) => err,
+        );
+    }
 
     render() {
-      if (!this.state.show) {
+      const {
+        show,
+        branches,
+        branchSelected,
+        isOpen,
+        filesSelected,
+        files,
+      } = this.state;
+      if (!show) {
         return null;
       }
-
+      console.log(filesSelected);
       document.getElementsByTagName('body').item(0).style.overflow = 'hidden';
       return (
         <div className="generic-modal">
@@ -83,7 +95,11 @@ class SelectDataPipelineModal extends Component {
                 <p>Select data to pre-process in your current data pipeline.</p>
               </div>
               <div id="x-button-div">
-                <button onClick={this.handleCloseButton} className="light-green-button">
+                <button
+                  type="button"
+                  onClick={this.handleCloseButton}
+                  className="light-green-button"
+                >
                   {' '}
                   <b>X</b>
                   {' '}
@@ -102,13 +118,11 @@ class SelectDataPipelineModal extends Component {
                 >
                   <ArrowButton
                     placeholder={
-                                    this.state.branchSelected
-                                      ? this.state.branchSelected
-                                      : 'Select branch'
-                                }
+                      branchSelected || 'Select branch'
+                    }
                     imgPlaceHolder={traiangle01}
                   />
-                  {this.state.isOpen && (
+                  {isOpen && (
                   <div className="select-branch" style={{ top: '27%', left: '35px' }} onBlur={this.handleBlur}>
                     <div
                       style={{
@@ -129,12 +143,12 @@ class SelectDataPipelineModal extends Component {
                       <div className="branches">
                         <ul>
                           <li className="branch-header">Branches</li>
-                          {this.state.branches.filter((branch) => !branch.name.startsWith('data-pipeline')
-                                                    && !branch.name.startsWith('experiment'))
+                          {branches.filter((branch) => !branch.name.startsWith('data-pipeline')
+                              && !branch.name.startsWith('experiment'))
                             .map((branch, index) => (
                               <li
                                 key={index}
-                                onClick={(e) => {
+                                onClick={() => {
                                   this.getFiles(branch.name);
                                 }}
                               >
@@ -151,18 +165,25 @@ class SelectDataPipelineModal extends Component {
               </div>
               <div id="right-div">
                 <button
+                  type="button"
                   className="light-green-button"
                   onClick={(e) => {
-                    this.props.handleModalAccept(e, this.state.filesSelected, this.state.branchSelected);
+                    this.props.handleModalAccept(e, filesSelected, this.state.branchSelected);
                   }}
                 >
-                                Accept
+                  Accept
                 </button>
-                <button className="white-button round-border-black-color">
+                <button
+                  type="button"
+                  className="white-button round-border-black-color"
+                >
                   {' '}
                   <p> Diselect all </p>
                 </button>
-                <button className="white-button round-border-black-color">
+                <button
+                  type="button"
+                  className="white-button round-border-black-color"
+                >
                   {' '}
                   <p> Select all </p>
                 </button>
@@ -180,22 +201,27 @@ class SelectDataPipelineModal extends Component {
                     <th style={{ width: '87%' }}>
                       <p>
                         {' '}
-                        { this.state.filesSelected.length }
+                        { filesSelected.length }
                         {' '}
-files selected
+                        files selected
                       </p>
                     </th>
                   </tr>
                 </thead>
 
                 <tbody>
-                  {this.state.files.map((file, index) => (
+                  {files.map((file, index) => (
                     <tr key={index} id={`tr-file-${index}`} className="files-row" style={{ justifyContent: 'unset' }}>
                       <td className="file-type" style={{ width: 'unset' }}>
-                        <label className="customized-checkbox">
-                          <input type="checkbox" checked={this.state.filesSelected.includes(file)} onChange={() => {}} />
-                          <span id={`span-file-${index}`} onClick={(e) => { this.selectFileFromGrid(e); }} className="checkmark" />
-                        </label>
+                        <Checkbox
+                          id={`span-file-${index}`}
+                          checked={filesSelected.includes(file)}
+                          onChange={(e) => { this.selectFileFromGrid(e); }}
+                          color="primary"
+                          inputProps={{
+                            'aria-label': 'primary checkbox',
+                          }}
+                        />
                       </td>
                       <td className="file-type" style={{ width: 'unset' }}>
                         <p>
@@ -218,6 +244,10 @@ files selected
 
 SelectDataPipelineModal.propTypes = {
   show: PropTypes.bool,
+};
+
+SelectDataPipelineModal.defaultProps = {
+  show: false,
 };
 
 export default SelectDataPipelineModal;
