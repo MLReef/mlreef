062077d40067cc61439ac40cf9335d7c333b89bf
SaathvikT <saathvik1997@gmail.com>
2019-11-28 13:19:28 +0000

    Bug/165 commits view error

--- web/src/__tests__/fileView.test.jsx
+++ web/src/__tests__/fileView.test.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import FileView from '../components/fileView/fileView';
+import { FileView } from '../components/fileView/fileView';
 
 Enzyme.configure({ adapter: new Adapter() });
 
