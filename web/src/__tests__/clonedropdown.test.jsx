0f04f1c6b40a289c3633eea765d2a0b8dbe43d73
Diego Viniegra <diego.viniegra@gmail.com>
2020-02-10 10:29:23 +0000

    fix: src/.env values
    Local environment was failing due typos here.

---
+++ web/src/__tests__/clonedropdown.test.jsx
@@ -0,0 +1,22 @@
+/* eslint-disable no-undef */
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { Clonedropdown } from '../components/projectInfo.jsx';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+const ssh = 'ssh://git@gitlab:10022/mlreef/walking-on-moon.git';
+const http = 'http://gitlab/mlreef/walking-on-moon.git';
+
+const wrapper = shallow(
+  <Clonedropdown http={http} ssh={ssh} />
+);
+
+describe('Component exists', () => {
+  it('should render 2 buttons when clicked', () => {
+    wrapper.find('.toggle-btn').simulate('click');
+
+    expect(wrapper.find('.clone-icon')).toHaveLength(2);
+  });
+});
