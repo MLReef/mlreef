b0fbbdbbde87cea8b0ec128ddd7ef4c05f765fc1
Diego Viniegra <diego.viniegra@gmail.com>
2020-06-08 12:46:43 -0500

    Fix connect to alt API during dev

--- web/src/setupProxy.js
+++ web/src/setupProxy.js
@@ -1,27 +1,33 @@
+/* eslint-disable */
 const proxy = require('http-proxy-middleware');
 
 // in production, "/api" should also happen to be redirected! If it is not, then you are not running the nginx-proxy!
 // then this middleware must proxy  develop frontend (npm start) to the backend
 // proxy
+
+const LOCALHOST = 'http://localhost';
+
 module.exports = function (app) {
-    const BACKEND_REROUTE = process.env.REACT_APP_BACKEND_REROUTE || 'http://localhost';
+  const BACKEND_REROUTE = process.env.REACT_APP_BACKEND_REROUTE || LOCALHOST;
+  const isLocalhost = BACKEND_REROUTE === LOCALHOST;
+
   app.use(
     // This is MLReef's own API served by our backend
     '/api/v1',
     proxy({
-      target: `${BACKEND_REROUTE}:8080`,
+      target: `${BACKEND_REROUTE}${isLocalhost ? ':8080' : ''}`,
       changeOrigin: true,
     })
-  ),
+  );
+
   app.use(
     // This is the Gitlab API served by the Gitlab container
     '/api/v4',
     proxy({
-      target: `${BACKEND_REROUTE}:10080`,
+      target: `${BACKEND_REROUTE}${isLocalhost ? ':10080' : ''}`,
       changeOrigin: true,
     })
   );
 };
 
 // start your local proxy,backend and gitlab!
-
