73b8f7c849973895394641096999d8f5c8119ba6
Andres Ausecha <andres@aamsis.co>
2020-08-10 18:24:25 +0000

    pass the status properly
    (cherry picked from commit 1c3f5ac0b5500afbe22ce6d2b65485a2d5c3296a)
    
    
    Former-commit-id: 0ab0a7aec9057def7c2c449a0124440b84e191dd [formerly d78232e8121419d52ab1fe5204e6e163534af8f1]
    
    Former-commit-id: 7dfc09d8e75ca783c830509169a2e2a0fc11ad61

--- web/src/components/data-instance/dataInstanceDetails.jsx
+++ web/src/components/data-instance/dataInstanceDetails.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import { toastr } from 'react-redux-toastr';
 import { connect } from 'react-redux';
+import { RUNNING, PENDING } from 'dataTypes';
 import FilesTable from '../files-table/filesTable';
 import Navbar from '../navbar/navbar';
 import ProjectContainer from '../projectContainer';
-import './dataInstanceDetails.css';
+import './dataInstanceDetails.scss';
 import FilesApi from '../../apis/FilesApi.ts';
+
+const filesApi = new FilesApi();
 
 const DataInstanceDetails = (props) => {
   const {
@@ -21,27 +24,26 @@ const DataInstanceDetails = (props) => {
     },
     location: {
       state: {
-        di_name: diName,
+        diName,
         timeCreatedAgo,
-      }
-    }
+        diStatus,
+      },
+    },
   } = props;
+
   const pipelineName = decodeURIComponent(diName);
   const selectedPipeline = branches.filter((item) => item.name === pipelineName);
   const groupName = project.namespace.name;
   const [files, setFiles] = useState([]);
 
   useEffect(() => {
-    const filesApi = new FilesApi();
     filesApi.getFilesPerProject(
       gid,
       path || '',
       false,
       diName,
     ).then((filesPerProject) => setFiles(filesPerProject))
-    .catch(() => {
-      toastr.error('Error', 'Something went wrong getting your files');
-    });
+      .catch(() => toastr.error('Error', 'Something went wrong getting your files'));
   }, [diName, gid, path]);
 
   return (
@@ -90,29 +92,46 @@ const DataInstanceDetails = (props) => {
           )}
         </div>
         <br />
-        <FilesTable
-          isReturnOptVisible={false}
-          files={files.map((f) => ({ id: f.id, name: f.name, type: f.type }))}
-          headers={[
-            'Name',
-          ]}
-          onClick={(e) => {
-            const target = e.currentTarget;
-            const targetDataKey = target.getAttribute('data-key');
-            const targetId = target.id;
-            const file = files.filter((f) => f.id === targetId)[0];
-            let link = '';
-            let routeType = '';
-            if (targetDataKey === 'tree') {
-              routeType = 'path';
-              link = `/my-projects/${gid}/${branch}/data-instances/${diName}/${routeType}/${encodeURIComponent(file.path)}`;
-            } else {
-              routeType = 'blob';
-              link = `/my-projects/${gid}/${diName}/${routeType}/${encodeURIComponent(file.path)}`;
-            }
-            props.history.push(link);
-          }}
-        />
+        {diStatus === RUNNING.toLowerCase() || diStatus === PENDING.toLowerCase() ? (
+          <>
+            <table className="file-properties">
+              <thead>
+                <tr className="title-row">
+                  <td style={{ marginLeft: '5rem' }}><p>Name</p></td>
+                </tr>
+              </thead>
+            </table>
+            <div id="empty-table">
+              <p>
+                No files can be shown until the pipeline has finished
+              </p>
+            </div>
+          </>
+        ) : (
+          <FilesTable
+            isReturnOptVisible={false}
+            files={files.map((f) => ({ id: f.id, name: f.name, type: f.type }))}
+            headers={[
+              'Name',
+            ]}
+            onClick={(e) => {
+              const target = e.currentTarget;
+              const targetDataKey = target.getAttribute('data-key');
+              const targetId = target.id;
+              const file = files.filter((f) => f.id === targetId)[0];
+              let link = '';
+              let routeType = '';
+              if (targetDataKey === 'tree') {
+                routeType = 'path';
+                link = `/${project.namespace}/${project.slug}/-/tree/${branch}/${encodeURIComponent(file.path)}`;
+              } else {
+                routeType = 'blob';
+                link = `/my-projects/${gid}/${diName}/${routeType}/${encodeURIComponent(file.path)}`;
+              }
+              props.history.push(link);
+            }}
+          />
+        )}
       </div>
     </div>
   );
