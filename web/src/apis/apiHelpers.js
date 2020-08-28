95ea820d8a005fc0da7c093daa46ee4141db6306
Diego Viniegra <diego.viniegra@gmail.com>
2020-08-28 20:51:10 +0000

    Improve filter
    By instanciating the regexp only once.
    
    
    Former-commit-id: d296c71018aa262ae369aab2906ea5b91e6c5e8a [formerly 57c764c81bcc477861cedb3c1f626eef72242c55]
    
    Former-commit-id: 4bb1381775f32aa0f35dcbbce849e23e5bccf839

--- web/src/apis/apiHelpers.js
+++ web/src/apis/apiHelpers.js
@@ -33,4 +33,12 @@ export const getDomain = () => {
   const hostname = window?.location?.hostname;
 
   return domain || `${protocol}//${hostname}`;
-}
+};
+
+export const filterBots = (users) => {
+  const bot = /bot$/;
+  return users
+    .filter((user) => !bot.test(user.username) && !bot.test(user.user_name));
+};
+
+export const filterRoot = (users) => users.filter((user) => user.id !== 1);
