8460e7c5533ce1d70e5d8d428b7b24b6be05098b
SaathvikT <saathvik1997@gmail.com>
2020-10-12 14:41:59 +0000

    Display images inside markdown files

--- web/src/components/ReadMe/ReadMe.jsx
+++ web/src/components/ReadMe/ReadMe.jsx
@@ -1,14 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import { toastr } from 'react-redux-toastr';
 import { string, number } from 'prop-types';
-import ReactMarkdown from 'react-markdown';
+import ReactMarkdown from 'react-markdown/with-html';
 import './Readme.scss';
 import { Base64 } from 'js-base64';
+import { getRootUrl } from 'apis/apiHelpers';
 import FilesApi from '../../apis/FilesApi.ts';
 
 const filesApi = new FilesApi();
 
-const Readme = ({ projectId, branch }) => {
+const Readme = ({ projectId, branch, namespace, slug }) => {
   const [textContent, setTextContent] = useState('');
   useEffect(() => {
     filesApi.getFileData(
@@ -19,6 +20,15 @@ const Readme = ({ projectId, branch }) => {
       .then((res) => setTextContent(Base64.decode(res.content)))
       .catch((error) => toastr.error('Error', error.message));
   }, [projectId, branch]);
+
+  const modifyImageSource = (input) => {
+    const relToAbsPath = new URL(input, `${getRootUrl()}:10080/${namespace}/${slug}/raw/${branch}/`).href;
+    if (/^https?:/.test(input)) {
+      return input;
+    }
+    return relToAbsPath;
+  };
+
   return (
     <div className="readme-container">
       <div className="readme-titlebar">
@@ -29,7 +39,11 @@ const Readme = ({ projectId, branch }) => {
       </div>
 
       <div className="file-preview md">
-        <ReactMarkdown source={textContent} />
+        <ReactMarkdown
+          source={textContent}
+          escapeHtml={false}
+          transformImageUri={(uri) => modifyImageSource(uri)}
+        />
       </div>
     </div>
   );
@@ -38,11 +52,15 @@ const Readme = ({ projectId, branch }) => {
 Readme.defaultProps = {
   projectId: null,
   branch: '',
+  namespace: '',
+  slug: '',
 };
 
 Readme.propTypes = {
   projectId: number,
   branch: string,
+  namespace: string,
+  slug: string,
 };
 
 export default Readme;
