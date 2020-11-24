5f42f83910081dd16fb709ffbeab9ce2e364b2b1
Rainer Kern <rainer@systemkern.com>
2020-11-24 17:13:47 +0100

    Add remark plugin configuration

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -50,5 +50,13 @@ module.exports = {
       //   ]
       // }
     },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          'gatsby-remark-mermaid'
+        ]
+      }
+    },
   ]
 }
