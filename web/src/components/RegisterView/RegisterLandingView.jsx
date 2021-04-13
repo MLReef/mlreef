2da3e6b8691de8d5fccbf4d3482efebe85b19e7d
Andres Ausecha <andres@aamsis.co>
2021-04-13 17:32:04 +0000

    Add analytics tool to the FE

--- web/src/components/RegisterView/RegisterLandingView.jsx
+++ web/src/components/RegisterView/RegisterLandingView.jsx
@@ -2,12 +2,13 @@ import React, { useState, useMemo } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'router';
 import PropTypes from 'prop-types';
+import TagManager from 'react-gtm-module';
 import MSelect from 'components/ui/MSelect';
 import { tutorials } from 'components/commons/Tutorial/data.json';
 import './RegisterView.scss';
 
 const RegisterLandingView = (props) => {
-  const { user: { userInfo: { name } } } = props;
+  const { user: { userInfo: { name, id } } } = props;
 
   const [tutorialId, setTutorialId] = useState(tutorials[0]?.id);
 
@@ -64,22 +65,37 @@ const RegisterLandingView = (props) => {
             />
           )}
         </div>
-        <div className="user-options">
-          <Link className="option-link mb-3 danger-box" to="/#explore">
-            <div className="option-icon" style={{ backgroundColor: 'var(--danger)' }} />
-            <div className="state-body">
-              <h4 className="state-title mt-0">Explore Public Projects</h4>
-              <p className="state-text">
+        <div
+          className="user-options"
+          onClick={(e) => {
+            const date = new Date();
+            const tagManagerArgs = {
+              gtmId: 'GTM-K8HPDBL',
+              events: {
+                view: 'WelcomePage',
+                userId: id,
+                ref: e.target.id,
+                timestamp: date.toString()
+              },
+            };
+            TagManager.initialize(tagManagerArgs);
+          }}
+        >
+          <Link className="option-link mb-3 danger-box" to="/dashboard/ml-project#explore">
+            <div id="explore-link-icon" className="option-icon" style={{ backgroundColor: 'var(--danger)' }} />
+            <div id="explore-link-content" className="state-body">
+              <h4 id="explore-link-title" className="state-title mt-0">Explore Public Projects</h4>
+              <p id="explore-link-text-content" className="state-text">
                 Public projects are an easy way to get started. Explore the ever
                 growing number of projects, get inspired and be part of the reef
               </p>
             </div>
           </Link>
           <Link className="option-link mb-3 info-box" to="/new-project/classification/ml-project">
-            <div className="option-icon" style={{ backgroundColor: 'var(--dark)' }} />
-            <div className="state-body pl-3">
-              <h4 className="state-title mt-0">Create a Project</h4>
-              <p className="state-text mt-0">
+            <div id="new-project-link-icon" className="option-icon" style={{ backgroundColor: 'var(--dark)' }} />
+            <div id="new-project-link-content" className="state-body pl-3">
+              <h4 id="new-project-link-title" className="state-title mt-0">Create a Project</h4>
+              <p id="new-project-link-text-content" className="state-text mt-0">
                 In MLReef there are ML projects, which host your data and experiments.
                 Furthermore you can create code only projects for data operations, data
                 visualizations and ML models.
@@ -87,19 +103,19 @@ const RegisterLandingView = (props) => {
             </div>
           </Link>
           <Link className="option-link mb-3 success-box" to="/groups/new">
-            <div className="option-icon" style={{ backgroundColor: 'var(--success)' }} />
-            <div className="state-body">
-              <h4 className="state-title mt-0">Create a Group</h4>
-              <p className="state-text">
+            <div id="new-group-link-icon" className="option-icon" style={{ backgroundColor: 'var(--success)' }} />
+            <div id="new-group-link-content" className="state-body">
+              <h4 id="new-group-link-title" className="state-title mt-0">Create a Group</h4>
+              <p id="new-group-link-text-content" className="state-text">
                 In a group you can manage shared projects and coordinate with your group members.
               </p>
             </div>
           </Link>
           <a className="option-link mb-3 warning-box" href="https://doc.mlreef.com/" target="_blank" rel="noopener noreferrer">
-            <div className="option-icon" style={{ backgroundColor: 'var(--warning)' }} />
-            <div className="state-body">
-              <h4 className="state-title mt-0">Learn more about MLReef</h4>
-              <p className="state-text">
+            <div id="documentation-link-icon" className="option-icon" style={{ backgroundColor: 'var(--warning)' }} />
+            <div id="documentation-link-content" className="state-body">
+              <h4 id="documentation-link-title" className="state-title mt-0">Learn more about MLReef</h4>
+              <p id="documentation-link-text-content" className="state-text">
                 Take a look at the documentation to know about
                 all capabilities of MLReef
               </p>
