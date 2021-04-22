2cbfd0ae5c343caaf3706292104804faa69eb1e8
Erika Torres <erika.torres@mlreef.com>
2021-04-22 11:45:15 +0000

    Fix/remove packages idl

--- backend/mlreef-domain/src/main/kotlin/com/mlreef/rest/BaseEnvironment.kt
+++ backend/mlreef-domain/src/main/kotlin/com/mlreef/rest/BaseEnvironment.kt
@@ -19,30 +19,10 @@ enum class BaseEnvironment(
 ) {
     UNDEFINED("", "", "", "", "", ""),
     PYTHON_3_6_GPU(
-        title = "Base environment python-3.6-GPU",
+        title = "Base environment python3, GPU and TF 2.1.0",
         docker_image = "tensorflow/tensorflow:2.1.0-gpu-py3",
-        description = "GPU nvidia drivers and TF 2.1, python 3.6",
-        requirements = """
-            |cycler==0.10.0
-            |decorator==4.4.2
-            |imageio==2.8.0
-            |joblib==0.14.1
-            |kiwisolver==1.2.0
-            |matplotlib==3.2.1
-            |networkx==2.4
-            |numpy==1.18.4
-            |nltk==3.5
-            |num2words==0.5.10
-            |opencv-python==4.2.0.34
-            |Pillow==7.1.2
-            |pyparsing==2.4.7
-            |python-dateutil==2.8.1
-            |PyWavelets==1.1.1
-            |scikit-image==0.16.2
-            |scikit-learn==0.23.1
-            |scipy==1.4.1
-            |six==1.14.0
-            """.trimMargin(),
+        description = "GPU nvidia drivers and TF 2.1, compatible with python 3.6-3.8",
+        requirements = "".trimMargin(),
         machine_type = "GPU",
         python_version = "3.6"
     );
