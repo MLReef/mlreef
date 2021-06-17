728ef248d7c8dfc67f68fa485536603ed729d241
Andres Ausecha <andres@aamsis.co>
2021-06-17 07:20:02 +0000

    Add a plugin and a custom component to handle PDF files

---
+++ web/src/__tests__/layout/MPDFReader/MPDFReader.test.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MPDFFilesReader from 'components/layout/MPdfReader/MPDFFilesReader';
+import { act } from 'react-dom/test-utils';
+import { sleep } from 'functions/testUtils';
+
+const fs = require('fs');
+let container;
+let base64Data;
+
+fs.readFile('src/__tests__/layout/MPDFReader/base64.txt', (err, data) => {
+  if (err) console.log(err);
+
+  base64Data = data.toString();
+});
+
+describe('', () => {
+  beforeEach(() => {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+  });
+  afterEach(() => {
+    document.body.removeChild(container);
+    container = null;
+  });
+  test('', async () => {
+    expect.assertions(3);
+    await sleep(250);
+    act(() => {
+      ReactDOM.render(<MPDFFilesReader data={base64Data}/> , container);
+    });
+    expect(document.getElementsByTagName('canvas')).toBeDefined();
+    expect(document.getElementsByTagName('button').length).toBe(2);
+    expect(document.getElementsByTagName('input').length).toBe(1);
+    await sleep(50);
+  });
+});
