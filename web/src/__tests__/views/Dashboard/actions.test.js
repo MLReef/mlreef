e48d933bb5c71b3661e4f15ca24ac3b16385876d
Andres Ausecha <andres@aamsis.co>
2021-06-22 14:45:42 +0000

    Make insights view look friendly for forked projects

--- web/src/__tests__/views/Dashboard/actions.test.js
+++ web/src/__tests__/views/Dashboard/actions.test.js
@@ -32,3 +32,11 @@ describe('test  functions', () => {
     });
   });
 });
+
+test('assert that body generation works changing the name', () => {
+  expect(buildProjectsRequestBodyV2(
+    'all', [], 'jaja project', 0, true,
+  )).toStrictEqual(
+    { visibility: 'PUBLIC', input_data_types_or: [], name: 'jaja project', published: true },
+  );
+});
