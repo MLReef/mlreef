6dfaf99ac21755ea40d622579838475f4a8d4cf3
German Sidorenko <german.sidorenko@gmail.com>
2021-07-02 08:14:07 +0000

    For FE test of OAuth

--- backend/mlreef-rest/src/main/kotlin/com/mlreef/rest/config/security-services.kt
+++ backend/mlreef-rest/src/main/kotlin/com/mlreef/rest/config/security-services.kt
@@ -1,6 +1,7 @@
 package com.mlreef.rest.config
 
 import java.util.UUID
+import kotlin.random.Random
 
 fun String?.censor(): String? {
     if (this == null)
@@ -24,7 +25,26 @@ fun String?.tryToUUID(): UUID? {
     }
 }
 
+fun String?.maskSecret(showFirst: Int = 3, showLast: Int = 3, randomMaskLength: Boolean = true): String? {
+    if (this == null)
+        return null
 
+    if (this.length <= (showFirst + showLast)) return randomizeMaskLength("*".repeat(this.length), randomMaskLength)
+
+    val firstStr = if (this.length > showFirst) this.substring(0, showFirst) else "*".repeat(this.length)
+    val lastStr = if (this.length >= (showFirst + showLast)) this.substring(this.length - showLast, this.length) else "*".repeat(firstStr.length - this.length)
+    val middle = if (this.length > (showFirst + showLast)) "*".repeat(this.length - (showFirst + showLast)) else ""
+
+    return "$firstStr${randomizeMaskLength(middle, randomMaskLength)}$lastStr"
+}
+
+private fun randomizeMaskLength(mask: String, randomMaskLength: Boolean): String {
+    if (!randomMaskLength || mask.length <= 3) return mask
+
+    val newMaskLength = Random.nextInt(mask.length / 2, mask.length + mask.length / 2)
+
+    return "*".repeat(newMaskLength)
+}
 
 
 
