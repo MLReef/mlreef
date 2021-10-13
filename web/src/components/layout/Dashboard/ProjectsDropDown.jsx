bd4cee235c76680b7de42fc635b8a7dffb10b64e
campac <cpachmann@gmail.com>
2021-10-13 10:23:10 +0200

    Change names to modules

--- web/src/components/layout/Dashboard/ProjectsDropDown.jsx
+++ web/src/components/layout/Dashboard/ProjectsDropDown.jsx
@@ -12,7 +12,7 @@ export default () => {
         className="new-project btn-primary"
         onClick={toggleShow}
       >
-        New Project
+        New Module
         {' '}
         <i className={`fa fa-chevron-${isDropdownOpen ? 'up' : 'down'} my-auto`} />
       </button>
@@ -20,18 +20,18 @@ export default () => {
         <div className="dashboard-v2-content-search-bar-dropdown-content mt-1" style={{ top: `${dropDownRef.current.offsetTop + 42}px` }}>
           <hr className="m-0" />
           <Link to="/new-project/classification/model">
-            <h5 className="t-warning m-0 mb-1">New Model</h5>
-            <span>Create a model</span>
+            <h5 className="t-warning m-0 mb-1">New algorithm</h5>
+            <span>Algorithms module can be used in experiment pipelines</span>
           </Link>
           <hr className="m-0" />
           <Link to="/new-project/classification/data-operation">
-            <h5 className="t-danger m-0 mb-1">New Data operation</h5>
-            <span>Create a data operation</span>
+            <h5 className="t-danger m-0 mb-1">New data operation</h5>
+            <span>Data operations are pre-processing modules for DataOps pipelines</span>
           </Link>
           <hr className="m-0" />
           <Link to="/new-project/classification/data-visualization">
-            <h5 style={{ color: 'rgb(115, 93, 168)' }} className="m-0 mb-1">New Data visualization</h5>
-            <span>Create a data visualization</span>
+            <h5 style={{ color: 'rgb(115, 93, 168)' }} className="m-0 mb-1">New visualization</h5>
+            <span>Create modules for visualizing data</span>
           </Link>
         </div>
       )}
