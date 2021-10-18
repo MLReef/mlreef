15798b864956fccd6f376011122f91595ea1aac3
Cuahtli Guerrero <vocesvegetales@gmail.com>
2021-10-18 08:44:48 -0500

    Skip tests

--- web/src/__tests__/RegisterLandingView.test.jsx
+++ web/src/__tests__/RegisterLandingView.test.jsx
@@ -1,39 +1,39 @@
-// import React from 'react';
-// import { MemoryRouter } from 'react-router-dom';
-// import { shallow } from 'enzyme';
-// import renderer from 'react-test-renderer';
-// import RegisterLandingView from 'components/views/RegisterView/RegisterLandingView';
-// import { storeFactory } from '../functions/testUtils';
-// import { userProfileMock } from '../testData';
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
+import RegisterLandingView from 'components/views/RegisterView/RegisterLandingView';
+import { storeFactory } from '../functions/testUtils';
+import { userProfileMock } from '../testData';
 
-// const store = storeFactory({
-//   user: userProfileMock,
-// });
+const store = storeFactory({
+  user: userProfileMock,
+});
 
-// const setup = () => {
-//   const wrapper = shallow(
-//     <RegisterLandingView store={store} />,
-//   );
-//   const afterDive = wrapper.dive().dive();
-//   return afterDive;
-// };
+const setup = () => {
+  const wrapper = shallow(
+    <RegisterLandingView store={store} />,
+  );
+  const afterDive = wrapper.dive().dive();
+  return afterDive;
+};
 
-// describe('render basic landing view elements', () => {
-//   const wrapper = setup();
-//   test('assert that snapshot matches', () => {
-//     const snapShot = renderer.create(
-//       <MemoryRouter>
-//         <RegisterLandingView store={store} />
-//       </MemoryRouter>,
-//     ).toJSON();
-//     expect(snapShot).toMatchSnapshot();
-//   });
+describe('render basic landing view elements', () => {
+  const wrapper = setup();
+  xtest('assert that snapshot matches', () => {
+    const snapShot = renderer.create(
+      <MemoryRouter>
+        <RegisterLandingView store={store} />
+      </MemoryRouter>,
+    ).toJSON();
+    expect(snapShot).toMatchSnapshot();
+  });
 
-//   test('aseert that user name matches', () => {
-//     expect(wrapper.find('.title-lg').text()).toEqual('Welcome to MLReef@mlreef');
-//   });
+  test('aseert that user name matches', () => {
+    expect(wrapper.find('.title-lg').text()).toEqual('Welcome to MLReef@mlreef');
+  });
 
-//   test('assert all user options are displayed', () => {
-//     expect(wrapper.find('.option-link')).toHaveLength(4);
-//   });
-// });
+  test('assert all user options are displayed', () => {
+    expect(wrapper.find('.option-link')).toHaveLength(4);
+  });
+});
