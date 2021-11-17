60f73ec7ea900d60564d79053140d34c4af345fa
Cuahtli Guerrero <vocesvegetales@gmail.com>
2021-11-17 18:03:07 +0000

    Add logs for debugging social oauth

--- web/src/components/views/RegisterRedirectionView/RegisterRedirectionView.jsx
+++ web/src/components/views/RegisterRedirectionView/RegisterRedirectionView.jsx
@@ -4,53 +4,68 @@ import { toastr } from 'react-redux-toastr';
 import useHistory from 'router/useHistory';
 import { getWhoAmI } from 'store/actions/userActions';
 
-export const RegisterRedirectionView = () => {
+const RegisterRedirectionView = () => {
+  const [privateToken, setPrivateToken] = useState(null);
 
-    const [PrivateToken, setPrivateToken] = useState(null);
+  const dispatch = useDispatch();
+  const user = useSelector(state => state.user);
 
-    const dispatch = useDispatch();
-    const user = useSelector(state => state.user);
+  const history = useHistory();
 
-    const history = useHistory();
+  useEffect(() => {
+    const token = document.cookie.split('; ')
+      .find(cookie => cookie.includes('PRIVATE-TOKEN'))?.split('=')[1];
 
-    useEffect(() => {
-        const Token = document.cookie.split('; ').find(cookie => cookie.includes('PRIVATE-TOKEN'))?.split('=')[1];
-        if(Token){
-            setPrivateToken(Token);
-            dispatch(getWhoAmI());
-        }else{
-            toastr.warning('Warning: ', 'Login unsuccessful');
+    console.log({ token });
+
+    if (token) {
+      setPrivateToken(token);
+      dispatch(getWhoAmI());
+    } else {
+      toastr.warning('Warning: ', 'Login unsuccessful');
+    }
+  }, []);
+
+  useEffect(() => {
+    console.log({ privateToken, user });
+
+    if (privateToken && user.auth) {
+      toastr.success('Success:', 'Login successfully');
+      const now = new Date();
+      now.setMonth(now.getMonth() + 1);
+      document.cookie = `private_token=${user.access_token}; expires=${now.toUTCString()};`;
+
+      console.log(document.cookie);
+
+      const query = window.location.search.replace(/\?(.+)$/, '$1');
+
+      const redirect = query.split('&')
+        .map((chunk) => chunk.split('='))
+        .find((pair) => pair[0] === 'redirect');
+
+      console.log({ query, redirect });
+
+      if (redirect) {
+        switch (redirect[1]) {
+          case 'goback':
+            history.goBack();
+            break;
+
+          default:
+            history.push(`/${redirect}`);
+            break;
         }
-    }, []);
+      } else {
+        history.push('/');
+      }
+    }
+  }, [privateToken, user]);
 
-    useEffect(() => {
-        if(PrivateToken && user.auth){
-            toastr.success('Success:', 'Login successfully');
-            const now = new Date();
-            now.setMonth(now.getMonth() + 1);
-            document.cookie = `private_token=${user.access_token}; expires=${now.toUTCString()};`;
-            const query = window.location.search.replace(/\?(.+)$/, '$1');
-            const redirect = query.split('&')
-                .map((chunk) => chunk.split('='))
-                .find((pair) => pair[0] === 'redirect');
+  return (
+    <div>
+      Redirecting...
+    </div>
+  );
+};
 
-            if (redirect) {
-                switch (redirect[1]) {
-                case 'goback':
-                    return history.goBack();
-
-                default:
-                    return history.push(`/${redirect}`);
-                }
-            }
-
-            return history.push('/');
-        }
-    }, [PrivateToken, user])
-
-    return (
-        <div>
-            Redirecting...
-        </div>
-    )
-}
+export default RegisterRedirectionView;
