1f75ed75f43a16c3ad9fef14dd2fc22518bd5362
Andres Ausecha <andres@aamsis.co>
2021-12-16 17:18:20 +0000

    Repo clean up

--- web/src/__tests__/views/DetailedProjectView/DetailedProjectView.test.jsx
+++ web/src/__tests__/views/DetailedProjectView/DetailedProjectView.test.jsx
@@ -44,7 +44,7 @@ const setup = (auth) => {
   );
 };
 
-xdescribe('test basic rendering for data projects', () => {
+describe('test basic rendering for data projects', () => {
   test('assert that component renders for authenticathed users', async () => {
     const wrapper = setup(true);
     await sleep(500);
@@ -52,20 +52,20 @@ xdescribe('test basic rendering for data projects', () => {
 
     const authWrappers = wrapper.find('div.feature-list').find('AuthWrapper');
     expect(authWrappers.at(0).find('a#insights')).toHaveLength(1);
-    // expect(authWrappers.at(0).childAt(0).props().title).not.toBe('Please login');
+    expect(authWrappers.at(0).childAt(0).props().title).not.toBe('Please login');
 
-    // expect(wrapper.find('p#projectId').text().includes('5d005488-afb6-4a0c-852a-f471153a04b5')).toBeTruthy();
+    expect(wrapper.find('p#projectId').text().includes('5d005488-afb6-4a0c-852a-f471153a04b5')).toBeTruthy();
     const repoInfoLinks = wrapper.find('RepoInfo').find('Link');
     expect(repoInfoLinks.length).toBe(5);
 
-    // expect(repoInfoLinks.at(0).props().to).toBe('/mlreef/sign-language-classifier/-/commits/master');
-    // expect(repoInfoLinks.at(1).props().to).toBe('/mlreef/sign-language-classifier/-/branches');
-    // expect(repoInfoLinks.at(2).props().to).toBe('/mlreef/sign-language-classifier/-/merge_requests');
-    // expect(repoInfoLinks.at(3).props().to).toBe('/mlreef/sign-language-classifier/-/visualizations');
-    // expect(repoInfoLinks.at(4).props().to).toBe('/mlreef/sign-language-classifier/-/datasets');
+    expect(repoInfoLinks.at(0).props().to).toBe('/mlreef/sign-language-classifier/-/commits/master');
+    expect(repoInfoLinks.at(1).props().to).toBe('/mlreef/sign-language-classifier/-/branches');
+    expect(repoInfoLinks.at(2).props().to).toBe('/mlreef/sign-language-classifier/-/merge_requests');
+    expect(repoInfoLinks.at(3).props().to).toBe('/mlreef/sign-language-classifier/-/visualizations');
+    expect(repoInfoLinks.at(4).props().to).toBe('/mlreef/sign-language-classifier/-/datasets');
 
-    // expect(wrapper.find('RepoInfo').find('.stat-no').at(1).text()).toBe('6');
-    // expect(wrapper.find('FilesTable').find('tr').length).toBeGreaterThan(0);
+    expect(wrapper.find('RepoInfo').find('.stat-no').at(1).text()).toBe('6');
+    expect(wrapper.find('FilesTable').find('tr').length).toBeGreaterThan(0);
   });
 
   test('assert that fetch for visitors function is called', async () => {
@@ -75,23 +75,23 @@ xdescribe('test basic rendering for data projects', () => {
     const authWrappers = wrapper.find('div.feature-list').find('AuthWrapper');
 
     expect(authWrappers.at(0).find('a#insights')).toHaveLength(1);
-    // expect(authWrappers.at(0).childAt(0).props().title).toBe('Please login');
+    expect(authWrappers.at(0).childAt(0).props().title).toBe('Please login');
 
     const repoInfoAuthWrapper = wrapper.find('RepoInfo').find('AuthWrapper');
 
     expect(repoInfoAuthWrapper.at(0).find('p.visualizations-count')).toHaveLength(1);
-    // expect(repoInfoAuthWrapper.at(0).childAt(0).props().title).toBe('Please login');
+    expect(repoInfoAuthWrapper.at(0).childAt(0).props().title).toBe('Please login');
 
     expect(repoInfoAuthWrapper.at(1).find('p.datasets-count')).toHaveLength(1);
-    // expect(repoInfoAuthWrapper.at(1).childAt(0).props().title).toBe('Please login');
+    expect(repoInfoAuthWrapper.at(1).childAt(0).props().title).toBe('Please login');
 
     const repoFeaturesAuthWrapper = wrapper.find('RepoFeatures').find('AuthWrapper');
 
-    // expect(repoFeaturesAuthWrapper.at(1).find('a').text()).toBe('Data Ops');
-    // expect(repoFeaturesAuthWrapper.at(1).childAt(0).props().title).toBe('Please login');
+    expect(repoFeaturesAuthWrapper.at(1).find('a').text()).toBe('Data Ops');
+    expect(repoFeaturesAuthWrapper.at(1).childAt(0).props().title).toBe('Please login');
 
-    // expect(repoFeaturesAuthWrapper.at(2).find('a').text()).toBe('Data Visualization');
-    // expect(repoFeaturesAuthWrapper.at(2).childAt(0).props().title).toBe('Please login');
+    expect(repoFeaturesAuthWrapper.at(2).find('a').text()).toBe('Data Visualization');
+    expect(repoFeaturesAuthWrapper.at(2).childAt(0).props().title).toBe('Please login');
   });
 
   afterEach(() => {
